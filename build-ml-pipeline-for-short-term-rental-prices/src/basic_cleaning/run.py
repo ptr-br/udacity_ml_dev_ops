b293371a4d5c15d405985a4e847328b6a3beba1b
ptr-br <ptr-br@gmx.de>
2025-12-28 13:36:59 +0000

    improve data cleaning

--- build-ml-pipeline-for-short-term-rental-prices/src/basic_cleaning/run.py
+++ build-ml-pipeline-for-short-term-rental-prices/src/basic_cleaning/run.py
@@ -24,6 +24,7 @@ def go(args):
 
     # Drop outliers
     idx = df['price'].between(args.min_price, args.max_price)
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
 
     # Convert last_review to datetime
